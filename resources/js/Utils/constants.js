5c3226a56e9adc8fed8afb924943d74f4cf2f937
rifqipakeq <rifqirahardian2@gmail.com>
2025-07-22 13:19:42 +0700

    feat: add constants for categories, locations, prices, and sorting options

---
+++ resources/js/Utils/constants.js
@@ -0,0 +1,127 @@
+export const optionsCategories = [
+    {
+        label: "All Categories",
+        value: "all",
+    },
+    {
+        label: "Nature",
+        value: "nature",
+    },
+    {
+        label: "Culinary",
+        value: "culinary",
+    },
+    {
+        label: "Theme Park",
+        value: "theme-park",
+    },
+    {
+        label: "History & Education",
+        value: "history-education",
+    },
+    {
+        label: "Art & Culture",
+        value: "art-culture",
+    },
+];
+
+export const optionsLocations = [
+    {
+        label: "All Categories",
+        value: "all",
+    },
+    {
+        label: "Jabodetabek",
+        value: "jabodetabek",
+    },
+    {
+        label: "Jawa",
+        value: "jawa",
+    },
+    {
+        label: "Luar Jawa",
+        value: "outside-java",
+    },
+];
+
+export const optionsPriceRanges = [
+    {
+        label: "All Price",
+        value: "all",
+    },
+    {
+        label: "Di bawah Rp100.000",
+        value: "below-100000",
+    },
+    {
+        label: "Rp100.000 - Rp1.000.000",
+        value: "100000-1000000",
+    },
+    {
+        label: "Di atas Rp 1.000.000",
+        value: "2500000-5000000",
+    },
+];
+
+const optionsSorts = [
+    {
+        label: "Harga Terendah",
+        value: "lowest-price",
+    },
+    {
+        label: "Harga Tertinggi",
+        value: "hightest-price",
+    },
+    {
+        label: "Lokasi Terdekat",
+        value: "nearest",
+    },
+    {
+        label: "Lokasi Terjauh",
+        value: "farthest",
+    },
+];
+
+export const optionSortByDate = [
+    {
+        label: "Paling Baru",
+        value: "newest",
+    },
+    {
+        label: "Paling Lama",
+        value: "oldest",
+    },
+];
+
+export const categories = [
+    {
+        thumbnail: "/assets/category_mountain.svg",
+        name: "Mountains",
+        color: "bg-xgreen",
+    },
+    {
+        thumbnail: "/assets/category_beach.svg",
+        name: "Beaches",
+        color: "bg-xorange",
+    },
+    {
+        thumbnail: "/assets/category_shop.svg",
+        name: "Shop & Market",
+        color: "bg-xgray",
+    },
+    {
+        thumbnail: "/assets/category_history.svg",
+        name: "History & Education",
+        color: "bg-xpurple",
+    },
+    {
+        thumbnail: "/assets/category_artculture.svg",
+        name: "Art & Culture",
+        color: "bg-xred",
+    },
+    {
+        thumbnail: "/assets/category_themepark.svg",
+        name: "Theme Park",
+        color: "bg-xdarkgreen",
+    },
+];
